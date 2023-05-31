620606459ff0767fcde4a86532f34eaef678e834
AlviSid69 <dimondots@gmail.com>
2023-06-01 00:32:34 +0800

    Added Sidebar; Implemented Layout

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,6 +3,7 @@ import { classNames } from 'shared/lib/classNames/classNames';
 import { Navbar } from 'widgets/Navbar';
 import { AppRouter } from './providers/router';
 import './styles/index.scss';
+import { Sidebar } from 'widgets/Sidebar';
 
 
 const App = () => {
@@ -11,7 +12,10 @@ const App = () => {
   return (
     <div className={classNames('app', {}, [theme])}>
       <Navbar />
-      <AppRouter />
+      <div className='content-page'>
+        <Sidebar />
+        <AppRouter />
+      </div>
     </div>
   )
 }
