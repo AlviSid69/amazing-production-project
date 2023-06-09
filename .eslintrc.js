c65439431b26e629ba8ab3af3728810e3542b9b4
AlviSid69 <dimondots@gmail.com>
2023-06-09 21:00:03 +0800

    Added stylelint, i18n plugin for eslint, fixed all styles

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,8 @@ module.exports = {
     },
     "extends": [
         "eslint:recommended",
-        "plugin:react/recommended"
+        "plugin:react/recommended",
+        "plugin:i18next/recommended"
     ],
     "parser": "@typescript-eslint/parser",
     "parserOptions": {
@@ -16,8 +17,9 @@ module.exports = {
         "sourceType": 'module',
     },
     "plugins": [
+        "react",
         "@typescript-eslint",
-        "react"
+        "i18next"
     ],
     "rules": {
         'react/jsx-indent': [2, 2],
@@ -35,6 +37,7 @@ module.exports = {
         'import/extensions': 'off',
         'import/no-extraneous-dependencies': 'off',
         'no-underscore-dangle': 'off',
+        "i18next/no-literal-string": ['error', { markupOnly: true }]
     },
     globals: {
         __IS_DEV__: true,
